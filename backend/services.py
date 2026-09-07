8990b9828b1a3fe76e5ff1521ce9538e31eba175
Meenachin <132190870+Meenachin@users.noreply.github.com>
2026-09-07 20:07:13 +0530

    Update services.py

--- backend/services.py
+++ backend/services.py
@@ -1520,7 +1520,7 @@ def invoice_to_csv(invoices):
     def invoice_to_excel(invoices):
     
 
-    workbook = Workbook()
+        workbook = Workbook()
 
     # =========================================================
     # STYLES
